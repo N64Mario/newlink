a85a352ed3abbc4a3b0c1491673b2b3a15c9e430
unknown <goafucheng@gmail.com>
2023-10-17 14:11:46 +0400

    add the new link

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const [link, setLinks] = useState([
+    "https://chat.whatsapp.com/LOYFMFBjRge0MPeUJAhMGV",
+    "https://chat.whatsapp.com/KilAdPtmcZJKUxa5vhbPv0",
+  ]);
+
+  const [random, setRandom] = useState("");
+  const randomLinks = () => {
+    const random = Math.floor(Math.random() * link.length);
+    setRandom(link[random])
+  };
+  useEffect(() => {
+    randomLinks();
+    window.location.href = random;
+  });
+
+  return <div className="spinner"></div>;
+}
+
+export default App;
